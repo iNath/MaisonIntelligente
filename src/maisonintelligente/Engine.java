c8bc6e9a5450eff05bf08151835820f318f80604
Nathanaël Bertrand <cmoinath@gmail.com>
2013-12-14 14:07:27 +0100

    Implementation reel de la configuration des volets

--- src/maisonintelligente/Engine.java
+++ src/maisonintelligente/Engine.java
@@ -11,6 +11,7 @@ import maisonintelligente.acteur.IAmpoule;
 import maisonintelligente.acteur.IVolet;
 import maisonintelligente.acteur.IVoletListener;
 import maisonintelligente.acteur.VoletStub;
+import maisonintelligente.capteur.Configuration;
 import maisonintelligente.capteur.ConfigurationStub;
 import maisonintelligente.capteur.IConfiguration;
 import maisonintelligente.capteur.IConfigurationListener;
@@ -24,17 +25,23 @@ import maisonintelligente.capteur.PresenceStub;
 
 public class Engine implements Runnable {
 	
-	//IAmpoule ampouleService = new AmpouleStub();
-	IAmpoule ampouleService = new Ampoule();
+	IAmpoule ampouleService = new AmpouleStub();
+	//IAmpoule ampouleService = new Ampoule();
 	IVolet voletService = new VoletStub();
 	
-	IConfiguration configurationService = new ConfigurationStub();
+	
+	//IConfiguration configurationService = new ConfigurationStub();
+	IConfiguration configurationService = new Configuration();
+	
 	ILuminosite luminositeService = new LuminositeStub();
-	//IPresence presenceService = new PresenceStub();
-	IPresence presenceService = new Presence();
+	IPresence presenceService = new PresenceStub();
+	//IPresence presenceService = new Presence();
 	
 	private boolean isBusy = false;
+	private boolean stateChanged = false;
 	private IVoletListener voletListener;
+	
+	final private int SEUIL_LUMINOSITE = 70;
 	
 	public Engine(){
 		
@@ -44,59 +51,93 @@ public class Engine implements Runnable {
 		};
 		voletService.addListener(voletListener);
 		
-		doActions();
+		tryDoingActions();
 		
 		configurationService.addListener(new IConfigurationListener() {
 			public void configurationChanged() {
-				doActions();
+				stateChanged = true;
+				tryDoingActions();
 			}
 		});
 		luminositeService.addListener(new ILuminositeListener() {
 			public void luminositeChanged() {
-				doActions();
+				stateChanged = true;
+				tryDoingActions();
 			}
 		});
 		presenceService.addListener(new IPresenceListener() {
 			public void presenceChanged() {
-				doActions();
+				stateChanged = true;
+				tryDoingActions();
 			}
 		});
 	}
 
 	private void voletActionEndedHandler() {
-		voletService.removeListener(voletListener);
-		actionEndedHandler();
+		if(luminositeService.getLuminosite() >= SEUIL_LUMINOSITE){
+			voletService.removeListener(voletListener);
+			actionEndedHandler();
+		} else {
+			// Si on est toujours pas bon, on recommence le calcul
+			// A condition qu'on ai pas déjà tout ouvert..
+			if(ampouleService.getLumiere() < 100 && voletService.getOuvertureVolet() < 100){
+				doActions();
+			} else {
+				// Sinon on arrete la
+				actionEndedHandler();
+			}
+		}
 	}
 
 	private void actionEndedHandler() {
 		logState();
 		this.isBusy = false;
-		//if(hasNextAction) doActions();
+		
+		// Si l'etat de la maison a changé on relance les calculs
+		if(stateChanged){
+			tryDoingActions();
+		}
 	}
 
-	private synchronized boolean doActions(){
+	private synchronized boolean tryDoingActions(){
 		if(this.isBusy == true){
 			return false;
 		}
-		System.out.println("[Engine] Changement d'état commence");
-		
 		
 		this.isBusy = true;
+		
+		doActions();
+		
+		return true;
+	}
+	
+	private void doActions(){
+
+		System.out.println("[Engine] Changement d'état commence");
 		
 		if(!presenceService.estPresent()){
 			ampouleService.setLumiere(0);
 			voletService.setOuvertureVolet(0);
 		} else {
 			if(configurationService.estVoletOuvertActive()){
-				voletService.setOuvertureVolet(100);
-				ampouleService.setLumiere(50);
+				// Si les volets ne sont pas encore ouvert
+				if(voletService.getOuvertureVolet() == 0){
+					voletService.setOuvertureVolet(100);
+					ampouleService.setLumiere(50);
+				} else {
+					// Si les volets sont ouverts
+					
+					// Si la luminosité n'est toujours pas suffisante
+					if(luminositeService.getLuminosite() < SEUIL_LUMINOSITE){
+						// Alors on allume les lumieres a fond
+						ampouleService.setLumiere(100);
+					}
+				}
 			} else {
 				voletService.setOuvertureVolet(0);
 				ampouleService.setLumiere(100);
 			}
 		}
-		
-		return true;
 	}
 	
 	private void logState() {
---
+++ src/maisonintelligente/capteur/Configuration.java
@@ -0,0 +1,82 @@
+package maisonintelligente.capteur;
+
+import java.io.InputStream;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.ArrayList;
+
+import javax.json.Json;
+import javax.json.JsonObject;
+import javax.json.JsonReader;
+
+public class Configuration implements IConfiguration, Runnable {
+
+	ArrayList<IConfigurationListener> listeners = new ArrayList<IConfigurationListener>();
+	boolean modeActif;
+	
+	
+	
+	public Configuration() {
+		modeActif = false;
+		new Thread(this).start();
+	}
+
+	@Override
+	public boolean estVoletOuvertActive() {
+		
+		return modeActif;
+	}
+
+	@Override
+	public void addListener(IConfigurationListener listener) {
+		listeners.add(listener);
+	}
+
+	@Override
+	public void removeListener(IConfigurationListener listener) {
+		for(int i = 0; i<listeners.size();i++){
+			if(listeners.get(i).equals(listeners)){
+				listeners.remove(i);
+				break;
+			}
+		}
+	}
+
+	@Override
+	public void run() {
+		
+		while(true){
+			
+			boolean newModeActif = this.modeActif;
+			
+			// On va lire le fichier
+			URL url;
+			try {
+				url = new URL("http://localhost/maisonintelligente/get.php");
+			
+				InputStream is = url.openStream();
+				JsonReader rdr = Json.createReader(is);
+				JsonObject obj = rdr.readObject();
+				newModeActif = obj.getBoolean("valeur");
+			} catch(Exception e){
+				e.printStackTrace();
+			}
+			
+			if(newModeActif != this.modeActif){
+				this.modeActif = newModeActif;
+				System.out.println("Utilisateur a changé son mode à " + modeActif);
+			
+				for(int i=0;i<listeners.size();i++){
+					listeners.get(i).configurationChanged();
+				}
+			}
+			
+			try {
+				Thread.sleep(500);
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+		}
+	}
+
+}

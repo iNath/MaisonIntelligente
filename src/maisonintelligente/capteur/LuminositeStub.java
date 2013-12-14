5d01835c965d561367dbc71d4dab8a719f289af0
medi24 <mehdi-but@hotmail.fr>
2013-12-14 14:47:06 +0100

    luminosite done

--- src/maisonintelligente/capteur/LuminositeStub.java
+++ src/maisonintelligente/capteur/LuminositeStub.java
@@ -1,11 +1,47 @@
 package maisonintelligente.capteur;
+import static com.googlecode.javacv.cpp.opencv_highgui.cvSaveImage;
 
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
 import java.util.ArrayList;
+
+import javax.imageio.ImageIO;
+
+import com.googlecode.javacv.OpenCVFrameGrabber;
+import com.googlecode.javacv.cpp.opencv_core.IplImage;
 
 public class LuminositeStub implements ILuminosite, Runnable {
 	
 	ArrayList<ILuminositeListener> listeners = new ArrayList<ILuminositeListener>();
 	int luminosite;
+	
+	
+	
+	private static double getLuminositePixel(BufferedImage img, int x, int y) {
+		int argb = img.getRGB(x, y);
+		double Y;
+		int rgb[] = new int[] {
+		    (argb >> 16) & 0xff, //red
+		    (argb >>  8) & 0xff, //green
+		    (argb      ) & 0xff  //blue
+		};
+		Y = 0.2126*rgb[0] + 0.7152*rgb[1] + 0.0722*rgb[2];
+		return Y;
+		}
+	
+	private static void captureFrame() {
+        final OpenCVFrameGrabber grabber = new OpenCVFrameGrabber(0);
+        try {
+            grabber.start();
+            IplImage img = grabber.grab();
+            if (img != null) {
+                cvSaveImage("capture.jpg", img);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
 	
 	public LuminositeStub(){
 		luminosite = 50;
@@ -13,7 +49,26 @@ public class LuminositeStub implements ILuminosite, Runnable {
 	}
 	
 	@Override
-	public int getLuminosite() {
+	public int getLuminosite()  {
+		captureFrame();
+		BufferedImage img;
+		try {
+			img = ImageIO.read(new File("capture.png"));
+			 int w = img.getWidth();
+		        int h = img.getHeight();
+		        double L=0;
+		        for (int x=0; x<w; x++)
+		        	for (int y=0; y<h; y++)
+		        	{    		
+		        		L = L+ getLuminositePixel(img, x, y);      		
+		        	}
+
+		        luminosite= (int) (((L/(w*h))/255)*100);
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+       
 		return luminosite;
 	}
 
@@ -31,6 +86,7 @@ public class LuminositeStub implements ILuminosite, Runnable {
 			}
 		}
 	}
+	
 
 	@Override
 	public void run() {
